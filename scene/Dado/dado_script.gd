9fdda77dcea396ca102bd3d002dd3b5bc2c2d2b3
AndreaForneris <a.forneris.1745@vallauri.edu>
2024-01-25 15:44:50 +0100

    Aggiunta fisica e lancio dado.
    Trovato bug nella fisica del dado
    Possibile soluzione: upscale delle collision-box e dei modelli

--- scene/Dado/dado_script.gd
+++ scene/Dado/dado_script.gd
@@ -2,27 +2,34 @@ extends RigidBody3D
 class_name Dado
 
 #forza max e min per x,y,z
-@export var forzaMax = Vector3(-0.5,0,0.5)
-@export var forzaMin = Vector3(-2,-0.2,-0.5)
+@export var forzaMax = 10
+@export var forzaMin = 0.5
+@export var forzaRotaz = 30
 
-var forza = Vector3()
-var posizione= Vector3()
-
+var start_pos
 var lancia: bool = true
+
+func  _ready():
+	start_pos = position
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and lancia: 
 		lancia = false
 		freeze = false
 		visible = true
+		transform.origin = start_pos
 		
-		forza = Vector3(randf_range(forzaMin[0], forzaMax[0]),
-		randf_range(forzaMin[1], forzaMax[1]),
-		randf_range(forzaMin[2], forzaMax[2]))
+		linear_velocity = Vector3.ZERO
+		angular_velocity = Vector3.ZERO
 		
-		print(forza)
+		transform.basis = Basis(Vector3.RIGHT, randf_range(forzaMin,forzaMax * PI)) * transform.basis
+		transform.basis = Basis(Vector3.UP, randf_range(forzaMin,forzaMax * PI)) * transform.basis
+		transform.basis = Basis(Vector3.FORWARD, randf_range(forzaMin,forzaMax * PI)) * transform.basis
 		
-		apply_impulse(forza, Vector3(0,1,0))
+		var vettore_lancio = Vector3(randf_range(-1,-0.5), 0, 0).normalized()
+		angular_velocity = vettore_lancio * forzaRotaz/2
+		apply_central_impulse(vettore_lancio * forzaRotaz)
+		
 		$Timer.start()
 
 func _on_timer_timeout():
