6575b591344c3329a0d4b13b4a3e0b52b3ced8fb
AndreaForneris <a.forneris.1745@vallauri.edu>
2024-02-20 17:28:49 +0100

    Update spowner_script.gd

--- scene/Main/spowner_script.gd
+++ scene/Main/spowner_script.gd
@@ -2,9 +2,41 @@ extends Node3D
 
 @export var dice: PackedScene
 @export var nDadi = 5
+#
+#@onready var faccie = $Raycasts.get_children()
+#signal valFaccia(value)
+#
+#@onready var start_pos = position
+#var forzaMax = -3
+#var forzaMin = -2.0
+#var forzaRotaz = 5
+#
 
 func _ready():
 	for i in nDadi:
 		var dice = dice.instantiate()
 		dice.name = 'dado' + str(i+1)
 		add_child(dice)
+#func _physics_process(delta):
+	#if Input.is_action_just_pressed("ui_accept"): 
+		#freeze = false
+		#visible = true
+		#transform.origin = start_pos
+		#
+		#linear_velocity = Vector3.ZERO
+		#angular_velocity = Vector3.ZERO
+		#
+		#transform.basis = Basis(Vector3.RIGHT, randf_range(forzaMin,forzaMax * PI)) * transform.basis
+		#transform.basis = Basis(Vector3.UP, randf_range(forzaMin,forzaMax * PI)) * transform.basis
+		#transform.basis = Basis(Vector3.FORWARD, randf_range(forzaMin,forzaMax * PI)) * transform.basis
+		#
+		#var vettore_lancio = Vector3(randf_range(-1,-0.5), 0, 0).normalized()
+		#angular_velocity = vettore_lancio * forzaRotaz/2
+		#apply_central_impulse(vettore_lancio * forzaRotaz)
+#
+#
+#func _on_sleeping_state_changed():
+	#if sleeping:
+		#for faccia in faccie:
+			#if faccia.is_colliding():
+				#emit_signal("valFaccia", faccia.val_faccia)
