3638b1484673eeb24e80b58d4503309d27923095
AndreaForneris <a.forneris.1745@vallauri.edu>
2024-03-03 22:00:40 +0100

    Risolto parzialmente RD-19 (Vedere Jira per descrizione)

--- scene/Main/game_script.gd
+++ scene/Main/game_script.gd
@@ -1,13 +1,18 @@
 extends Node3D
 
 var rndNum: int
+var nDadiPlayer = 3
+var nDadiAI = 5
 
 func _ready():
 	rndNum = randi_range(1,6)
 	print("rnd num: "+str(rndNum))
-	
-	#for dado in $PlayerSpowner.get_children():
-		#print(str(dado) +": "+ str(dado.valore))
+	$AISpowner.loadDadi(nDadiAI)
+	$PlayerSpowner.loadDadi(nDadiPlayer)
 
-func risultato(value) -> void:
+#func _physics_process(delta):
+	#if Input.is_action_just_pressed("ui_accept"): 
+		#
+
+#func risultato(value) -> void:
 	
--- scene/Main/spowner_script.gd
+++ scene/Main/spowner_script.gd
@@ -1,19 +1,28 @@
 extends Node3D
 
 @export var dice: PackedScene
-var nDadi = 5
 
+var nDadi
 #vettore valore dadi
+var vetFacce :Array
 #contatore aggiunte
+var vetIndex
 
-func _ready():
+func loadDadi(value):
+	nDadi = value
+	vetFacce.resize(nDadi)
+	vetIndex = 0
 	for i in nDadi:
 		var dice = dice.instantiate()
 		dice.name = 'dado' + str(i+1)
 		add_child(dice)
 
 func getFacce(value) -> void:
-	print("getFacce"+str(value))
-	#add a vettore valore dadi
-	#se contatore = nDadi
-	#	get_parent().risultato(vettore)
+	#print("val Faccia: " + str(value))
+	vetFacce[vetIndex] = value
+	if vetIndex == nDadi:
+		for i in vetFacce:
+			print(str(i))
+		get_parent().risultato(vetFacce)
+	else:
+		vetIndex += 1
