a6cc11cd97cab49cf40c0700bd7bbe1a3c99a984
AndreaForneris <a.forneris.1745@vallauri.edu>
2024-03-26 19:16:44 +0100

    Implementato RD-15

--- scene/HUD/menu_script.gd
+++ scene/HUD/menu_script.gd
@@ -6,3 +6,7 @@ func _on_btn_instructions_pressed():
 
 func _on_btn_start_pressed():
 	get_tree().change_scene_to_file("res://scene/Main/game.tscn")
+
+
+func _on_btn_quit_pressed():
+	get_tree().quit()
