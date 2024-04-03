2b56b426f6464806b9215979511c1b4cb2d9bbd5
AndreaForneris <82877391+AndreaForneris@users.noreply.github.com>
2024-04-03 09:28:06 +0200

    Update
    Passare nDadi da lanciare come parametro a lanciadadi()

--- scene/Main/game_script.gd
+++ scene/Main/game_script.gd
@@ -1,50 +1,81 @@
 extends Node3D
 
 var rndNum: int
-var nDadiPlayer = 5
-var nDadiAI = 5
-
-func _ready():
-	rndNum = randi_range(1,6)
-	#impostaLabel(1, true, "Il numero estratto è : " + str(rndNum))
-	print("Il numero estratto è : " + str(rndNum))
+@onready var nDadiPlayer = 5
+@onready var nDadiAI = 5
+@onready var stato = "TrndAI"
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("Lancia"): 
-		#impostaLabel(1, false, "")
-		loadSpawners()
-		$PlayerSpowner.lanciaDadi()
-		$AISpowner.lanciaDadi()
+		risultato([0], "")
 	
 	if Input.is_action_just_pressed("Esci"):
 		get_tree().change_scene_to_file("res://scene/HUD/main_menu.tscn")
 
 func risultato(vet, name) -> void:
-	#selezione label
-	if name == "AISpowner":
-		#impostaLabel(1, true, name + " " + str(vet))
-		print(name + " " + str(vet)) 
-	else: 
-		#impostaLabel(2, true, name + " " + str(vet))
-		print(name + " " + str(vet))
+	match stato:
+		"TrndAI":#lancio dado 
+			#nDadiAI = 1 da passare come parametro a lancia dadi
+			loadSpawners()
+			$AISpowner.lanciaDadi()
+			stato = "RrndAI"
+		
+		"RrndAI":#lettura dado
+			rndNum = vet[0]
+			print("rnd: " + str(rndNum))
+			$Timer.start
+			if $Timer.is_stopped():
+				$PlayerSpowner.lanciaDadi()
+			stato = "playerGame"
+		
+		"playerGame":
+			for ris in vet:
+				if ris == rndNum:
+						nDadiPlayer -= 1
+						print("Palyer -1")
+			stato = "TrndPlayer"
+		
+		"TrndPlayer":#lancio dado 
+			nDadiPlayer = 1
+			loadSpawners()
+			$PlayerSpowner.lanciaDadi()
+			stato = "RrndPlayer"
+		
+		"RrndPlayer":#lettura dado
+			rndNum = vet[0]
+			print("rnd: " + str(rndNum))
+			stato = "AIGame"
+			$Timer.start
+			if $Timer.is_stopped():
+				$AISpowner.lanciaDadi()
+		
+		"AIGame":
+			for ris in vet:
+				if ris == rndNum:
+						nDadiAI -= 1
+						print("AI -1")
+			stato = "TrndAI"
 	
-	for ris in vet:
-		if ris == rndNum:
-			if name == "AISpowner":
-				nDadiPlayer -= 1
-				print("Palyer -1")
-				#impostaLabel(2, true, "Palyer -1")
-			else:
-				nDadiAI -= 1
-				print("AI -1")
-				#impostaLabel(2, true, "AI -1")
-	
-	if nDadiAI <= 0 and nDadiPlayer == nDadiAI:
-		print("pareggio")
-	if nDadiAI <= 0:
-		print("Palyer Vince")
-	if nDadiPlayer <= 0:
-		print("AI Vince")
+	#if name == "AISpowner":
+		#print(name + " " + str(vet)) 
+	#else: 
+		#print(name + " " + str(vet))
+	#
+	#for ris in vet:
+		#if ris == rndNum:
+			#if name == "AISpowner":
+				#nDadiPlayer -= 1
+				#print("Palyer -1")
+			#else:
+				#nDadiAI -= 1
+				#print("AI -1")
+	#
+	#if nDadiAI <= 0 and nDadiPlayer == nDadiAI:
+		#print("pareggio")
+	#if nDadiAI <= 0:
+		#print("Palyer Vince")
+	#if nDadiPlayer <= 0:
+		#print("AI Vince")
 
 func loadSpawners():
 	$AISpowner.clearContainer()
